57b81737a0c166d5b6831ec56ca44e71e61d1b93
Isabella Zhong <isabellazhongg@gmail.com>
2025-11-18 23:46:45 -0500

    task 1.1

--- src/main/java/theater/Constants.java
+++ src/main/java/theater/Constants.java
@@ -10,13 +10,13 @@ public final class Constants {
     public static final int COMEDY_EXTRA_VOLUME_FACTOR = 5;
     // comedy amount constants
     public static final int COMEDY_AMOUNT_PER_AUDIENCE = 300;
-    public final static int COMEDY_AUDIENCE_THRESHOLD = 20;
+    public static final int COMEDY_AUDIENCE_THRESHOLD = 20;
     public static final int COMEDY_BASE_AMOUNT = 30000;
     public static final int COMEDY_OVER_BASE_CAPACITY_AMOUNT = 10000;
     public static final int COMEDY_OVER_BASE_CAPACITY_PER_PERSON = 500;
     // tragedy amount constants
+    public static final int TRAGEDY_BASE_AMOUNT = 4000;
     public static final int TRAGEDY_AUDIENCE_THRESHOLD = 30;
-    public static final int TRAGEDY_BASE_AMOUNT = 40000;
     public static final int TRAGEDY_OVER_BASE_CAPACITY_PER_PERSON = 1000;
     // formatting constants
     public static final int PERCENT_FACTOR = 100;
@@ -30,6 +30,7 @@ public final class Constants {
     public static final int PASTORAL_OVER_BASE_CAPACITY_PER_PERSON = 2500;
     public static final int PASTORAL_AUDIENCE_THRESHOLD = 20;
     public static final int PASTORAL_VOLUME_CREDIT_THRESHOLD = 20;
+
 
     private Constants() {
 
